aade3ce13ac7d0ad602cc8deebb9f8cb28506d5d
michaelzheng1 <mzheng1@terpmail.umd.edu>
2019-12-02 19:16:27 -0500

    create getGradesFromProf

--- src/API/index.js
+++ src/API/index.js
@@ -8,6 +8,7 @@ var { mongoConnect } = require("./mongo/mongo");
 var getAllGrades = require('./routes/getAllGrades');
 var getAllProfessor = require("./routes/getAllProfessor");
 var getProfessor = require('./routes/getProfessor');
+var getGradesFromProf = require('./routes/getGradesFromProf');
 var fireProfessor = require('./routes/fireProfessor');
 var addGrade = require('./routes/addGrade');
 var addForm = require('./routes/addForm');
@@ -22,16 +23,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Route middleware
-app.use("/api/grades", getAllGrades);
-app.use("/api/professors", getAllProfessor);
-app.get("/api/professor/:name", (req, res) => { getProfessor(req, res) });
+app.use("/grades", getAllGrades);
+app.use("/professors", getAllProfessor);
+app.get("/professor/:name", (req, res) => { getProfessor(req, res) });
+app.get("/:name/grades", (req, res) => { getGradesFromProf(req, res) });
 
 // Post Routes
-app.use("/api/post/grade", addGrade);
-app.use("/api/post/form", addForm);
+app.use("/post/grade", addGrade);
+app.use("/post/form", addForm);
 
 //Delete Routes
-app.delete("/api/fire/:name", (req, res) => { fireProfessor(req, res) });
+app.delete("/fire/:name", (req, res) => { fireProfessor(req, res) });
 
 
 // Liston on port 3000
